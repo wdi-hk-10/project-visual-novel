715f28a527f887d65a0445c36d9c0ebba5c9bfe5
Daryl Chu <darylchu@me.com>
2016-01-25 23:38:11 +0800

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# project-visual-novel
+# The Last Man on Earth - a Visual Novel
+
+### Introduction
+This game is a visual novel. As you play the game, you will read a series of text before having an option to make a choice. That choice will alter the plot's ending. There are 3 endings.
+
+### Technologies
+- HTML
+- CSS
+- Bootstrap
+- Javascript
+- Jquery
--- js/story.js
+++ js/story.js
@@ -1,2 +1,11 @@
 $(document).ready(function(){
+
+/*
+Scenario A ('forever alone') will be the quickest way to end the game. This route contains 5 event CG to reach the worse ending.
+  - shares 2 CG with Scenario B and C
+Scenario B ('paradox') takes 6 event CG to reach the ending.
+  - shares 4 CG with Scenario C
+Scenario C ('netflix') is the longest way to end the game. This is the *best* ending. Takes 7 event CG to reach the best ending.
+*/
+
 });

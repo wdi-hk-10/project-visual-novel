0de08e0c00d055edc256da3917dd04e95f6160aa
Daryl Chu <darylchu@me.com>
2016-01-26 00:04:47 +0800

    fourth commit

--- js/story.js
+++ js/story.js
@@ -8,4 +8,16 @@ Scenario B ('paradox') takes 6 event CG to reach the ending.
 Scenario C ('netflix') is the longest way to end the game. This is the *best* ending. Takes 7 event CG to reach the best ending.
 */
 
+var scenarioA = {
+routeA:['cg1', 'cg2', 'cg3', 'cg4', 'cg5']
+};
+
+var scenarioB = {
+routeB:['cg1', 'cg2', 'cg6', 'cg7', 'cg8', 'cg9']
+};
+
+var scenarioC = {
+routeC:['cg1', 'cg2', 'cg6', 'cg7', 'cg10', 'cg11', 'cg12']
+};
+
 });

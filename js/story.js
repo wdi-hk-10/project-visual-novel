d2e1aee1c115154058767b939c77a1e2265f76fa
Daryl Chu <darylchu@me.com>
2016-01-26 13:55:21 +0800

    seventh commit

--- js/story.js
+++ js/story.js
@@ -1,22 +1,99 @@
 /*
 Scenario A ('forever alone') will be the quickest way to end the game. This route contains 5 event CG to reach the worse ending.
-  - shares 2 CG with Scenario B and C
-Scenario B ('paradox') takes 6 event CG to reach the ending.
-  - shares 4 CG with Scenario C
-Scenario C ('netflix') is the longest way to end the game. This is the *best* ending. Takes 7 event CG to reach the best ending.
+
+Scenario B ('paradox') takes 8 event CG to reach the ending.
+
+Scenario C ('netflix') is the longest way to end the game. This is the *best* ending. Takes 8 event CG to reach the best ending.
 */
 
-var scenarioA = {
-routeA:['cg1', 'cg2', 'cg3', 'cg4', 'cg5']
+var scene = function(option) {
+  this.img = option.img;
+  this.description = option.description;
+  this.routes = [];
 };
 
-var scenarioB = {
-routeB:['cg1', 'cg2', 'cg6', 'cg7', 'cg8', 'cg9']
-};
+// Scene Creation
+var scene1 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
 
-var scenarioC = {
-routeC:['cg1', 'cg2', 'cg6', 'cg7', 'cg10', 'cg11', 'cg12']
-};
+var scene2 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene3 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene4 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene5 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene6 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene7 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene8 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene9 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene10 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene11 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene12 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene13 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene14 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene15 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+var scene16 = new Scene({
+  img: '...',
+  description: '<h1>text</h1>'
+});
+
+//scene1.routes.push()
 
 $(document).ready(function(){
 
